3734abe330a80a5ccc9656a1646c38649eace501
marimikh <mburunduk@gmail.com>
2017-08-10 08:47:42 +0000

    "ruined test"

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -12,7 +12,7 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("fixme", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("ruined", app.items[0].name);
     }
 
 }

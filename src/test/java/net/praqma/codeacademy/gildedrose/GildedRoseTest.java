846682e3c26da5695f47a5403939eda871da08c3
marimikh <mburunduk@gmail.com>
2017-08-10 08:40:12 +0000

    " fixme fixed"

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -1,3 +1,4 @@
+
 package net.praqma.codeacademy.gildedrose;
 
 import static org.junit.Assert.*;
@@ -8,7 +9,7 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[] { new Item("fixme", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
